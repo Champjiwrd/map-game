81ed341865714232eb5983a782c147e1e9b7e5f8
Warodom Baiduom <champ0952@hotmail.com>
2024-08-29 15:39:16 +0700

    fix base url

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    baseURL: '/map-game/', // Ensure this matches your GitHub repository name
+  },
   compatibilityDate: '2024-04-03',
   ssr: false,
   devtools: { enabled: false },
@@ -16,7 +19,7 @@ export default defineNuxtConfig({
     // Options
     display: 'auto',
     families: {
-      Prompt: [100,200,300,400,500,600,700,800,900],
+      Prompt: [100, 200, 300, 400, 500, 600, 700, 800, 900],
     },
   },
 });
